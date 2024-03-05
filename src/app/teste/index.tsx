49e6b1bab09b0a7a8e8f51b7613461ec3c3fc6a5
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2024-03-05 15:33:00 -0300

    Arrow icon

--- src/app/teste/index.tsx
+++ src/app/teste/index.tsx
@@ -1,11 +1,23 @@
 import {View,Text,StatusBar} from 'react-native'
-
+import { MaterialIcons } from '@expo/vector-icons'
+import { router } from 'expo-router'
+import { style } from './styles'
 
 export default function Index(){
+    function handleBack(){
+    router.back()
+    }
+
     return(
-    <View>
+    <View style={style.container}>
     <StatusBar barStyle={'dark-content'}/>
-    <Text>Ola</Text>
+        <View>
+            <MaterialIcons
+            name='arrow-back'
+            onPress={handleBack}
+            size={40}
+            />
+        </View>
     </View>
     )
     
